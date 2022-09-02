45ba779a71f188e731b51ae0cde443717e205688
Pratyush Singh <aries.pratyush@gmail.com>
2022-09-02 16:57:22 +0530

    Added all the ops of a calc

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.button.MaterialButton
 import android.widget.TextView
 import android.widget.Button
-import org.mozilla.javascript.ContextFactory
-import java.lang.Exception
-import org.mozilla.javascript.ScriptableObject
+import java.util.Queue
+import java.util.LinkedList
 
-var stack=ArrayDeque<String>()
 var z:String=""
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,77 +39,99 @@ class MainActivity : AppCompatActivity() {
             txt.text = btnDot.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btnEqual.setOnClickListener {
-            getResult(btnEqual as MaterialButton)
+            var str= z+"/"
+            var temp="";
+            var nums: Queue<Int> = LinkedList<Int>()
+            var sym:  Queue<String> = LinkedList<String>()
+            for(i in 0..str.length-1){
+                if(str[i]>='0' && str[i]<='9')temp+=str[i]
+                else{
+                    // println("0000")
+                    sym.add(str[i].toString())
+                    nums.add(temp.toInt())
+                    // println(temp)
+                    temp=""
+                    println(nums.peek())
+                    println(sym.peek())
+                    if(nums.size==2){
+                        var a:Int=nums.remove()
+                        var b:Int=nums.remove();
+                        // println("$a $b ")
+                        var exp=sym.remove();
+                        if(exp=="+")nums.add(a+b)
+                        else if(exp=="-")nums.add(a-b)
+                        else if(exp=="*")nums.add(a*b)
+                        else if(exp=="/")nums.add(a/b)
+                        var s:String =nums.peek().toString();
+                        findViewById<TextView>(R.id.result_tv).text=s
+                    }
+                }
+                // println(nums.peek())
+                var s:String =nums.peek().toString();
+                findViewById<TextView>(R.id.result_tv).text=s
+            }
         }
         btnMinus.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btnMinus.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btnPlus.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btnPlus.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btnMul.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btnMul.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btnDiv.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btnDiv.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btnOpen.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btnOpen.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btnClose.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btnClose.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btn9.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn9.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btn8.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn8.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
-            findViewById<TextView>(R.id.solution_tv).setText(z);stack.addFirst(z)
+            findViewById<TextView>(R.id.solution_tv).setText(z)
         }
         btn7.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn7.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
-            findViewById<TextView>(R.id.solution_tv).setText(z);stack.addFirst(z)
+            findViewById<TextView>(R.id.solution_tv).setText(z)
         }
         btn6.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn6.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
-            findViewById<TextView>(R.id.solution_tv).setText(z);stack.addFirst(z)
+            findViewById<TextView>(R.id.solution_tv).setText(z)
         }
         btn5.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
@@ -123,67 +144,37 @@ class MainActivity : AppCompatActivity() {
             txt.text = btn4.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btn3.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn3.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btn2.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn2.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btn1.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn1.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btn0.setOnClickListener {
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = btn0.getText().toString()
             z+=findViewById<TextView>(R.id.solution_tv).getText().toString()
             findViewById<TextView>(R.id.solution_tv).setText(z)
-            stack.addFirst(z)
         }
         btnC.setOnClickListener {
-            stack.clear()
             z=""
             var txt = findViewById<TextView>(R.id.solution_tv)
             txt.text = "0"
-        }
-    }
-    fun getResult(btn:MaterialButton){
-        if(stack.size>0){
-            var a=stack.removeFirst();
-            stack.removeFirst();
-            var b=stack.removeFirst()
-            var c:Int=a.toInt(); var d:Int=b.toInt();
-            var txt = findViewById<TextView>(R.id.solution_tv)
-            txt.text = btn.getText().toString()
-            if(txt.text.equals("+")){
-                val res=c+d;
-                findViewById<TextView>(R.id.result_tv).setText(res)
-            }
-            else if(txt.text.equals("-")){
-                val res=c-d;
-                findViewById<TextView>(R.id.result_tv).setText(res)
-            }
-            else if(txt.text.equals("-")){
-                val res=c*d;
-                findViewById<TextView>(R.id.result_tv).setText(res)
-            }
-            else if(txt.text.equals("/")){
-                val res=c/d;
-                findViewById<TextView>(R.id.result_tv).setText(res)
-            }
+            var res=findViewById<TextView>(R.id.result_tv)
+            res.text="0"
         }
     }
 }

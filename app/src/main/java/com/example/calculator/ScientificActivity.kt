aec46fe03d94b02e4927e9e4edf8ed1d3bed104f
Pratyush Singh <aries.pratyush@gmail.com>
2022-09-05 19:45:06 +0530

    Added func of Sc calc

--- app/src/main/java/com/example/calculator/ScientificActivity.kt
+++ app/src/main/java/com/example/calculator/ScientificActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import java.util.*
 
 var str:String=""
 class ScientificActivity :AppCompatActivity(){
@@ -22,10 +23,10 @@ class ScientificActivity :AppCompatActivity(){
         val btn7 = findViewById<Button>(R.id.button_7)
         val btn8 = findViewById<Button>(R.id.button_8)
         val btn9 = findViewById<Button>(R.id.button_9)
-        val btnDiv = findViewById<Button>(R.id.button_div);
-        val btnMul = findViewById<Button>(R.id.button_mul);
-        val btnPlus = findViewById<Button>(R.id.button_plus);
-        val btnMinus = findViewById<Button>(R.id.button_minus);
+        val btnDiv = findViewById<Button>(R.id.button_div)
+        val btnMul = findViewById<Button>(R.id.button_mul)
+        val btnPlus = findViewById<Button>(R.id.button_plus)
+        val btnMinus = findViewById<Button>(R.id.button_minus)
         val btnEqual = findViewById<Button>(R.id.button_equal)
         val btnDot = findViewById<Button>(R.id.button_dot)
         val btnSqrt=findViewById<Button>(R.id.button_root)
@@ -36,11 +37,42 @@ class ScientificActivity :AppCompatActivity(){
             str=""
             findViewById<TextView>(R.id.solution_tv).text="0"
         }
+        btnEqual.setOnClickListener {
+            var x=str+"/";
+            var temp="";
+            var nums: Queue<Int> = LinkedList<Int>()
+            var sym: Queue<String> = LinkedList<String>()
+            for(i in 0..x.length-1){
+                if(x[i]>='0' && x[i]<='9')temp+=x[i]
+                else{
+                    sym.add(x[i].toString())
+                    nums.add(temp.toInt())
+                    // println(temp)
+                    temp=""
+                    println(nums.peek())
+                    println(sym.peek())
+                    if(nums.size==2){
+                        var a:Int=nums.remove()
+                        var b:Int=nums.remove();
+                        // println("$a $b ")
+                        var exp=sym.remove();
+                        if(exp=="+")nums.add(a+b)
+                        else if(exp=="-")nums.add(a-b)
+                        else if(exp=="*")nums.add(a*b)
+                        else if(exp=="/")nums.add(a/b)
+                        var s:String =nums.peek().toString();
+                        findViewById<TextView>(R.id.solution_tv).text=s
+                    }
+                }
+                var s:String =nums.peek().toString();
+                findViewById<TextView>(R.id.solution_tv).text=s
+            }
+        }
         extract(btn0);extract(btn1);extract(btn2);extract(btn3);extract(btn4);extract(btn5);extract(btn6);extract(btn7);
-        extract(btn8);extract(btn9);extract(btnDiv);extract(btnMul);extract(btnPlus);extract(btnMinus);extract(btnEqual);extract(btnDot);
+        extract(btn8);extract(btn9);extract(btnDiv);extract(btnMul);extract(btnPlus);extract(btnMinus);extract(btnDot);
         extract(btnSqrt);extract(btnSqr);extract(btnPowXY);extract(btnLog);
     }
-    fun extract(btn:Button){
+     fun extract(btn:Button){
         btn.setOnClickListener {
             var txt=findViewById<TextView>(R.id.solution_tv)
             txt.text=btn.getText().toString()
